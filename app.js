e0d07039294cfe6ed0b8da429b3c7ec32a37ed25
sazanote <484roeroi@gmail.com>
2021-12-10 20:54:14 +0900

    Feat: 목록에서 읽은 글 표시 기능 추가

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 const store = {
   currentPage: 1,
+  feeds: [],
 }
 function getData(url) {
   ajax.open('GET', url, false);
@@ -13,8 +14,16 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
+function makeFeeds(feeds) {
+  for (let i = 0; i < feeds.length; i++) {
+    feeds[i].read = false; //read 속성 추가
+  }
+
+  return feeds;
+}
+
 function newsFeed() {
-  const newsFeed = getData(NEWS_URL);
+  let newsFeed = store.feeds;
   const newsList = [];
   let template = `
     <div class="bg-gray-600 min-h-screen">
@@ -41,9 +50,13 @@ function newsFeed() {
     </div>
   `;
 
+  if(newsFeed.length === 0) {
+    newsFeed = store.feeds = makeFeeds(getData(NEWS_URL));
+  }
+
   for(let i = (store.currentPage -1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
-      <div class="p-6 bg-white mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+      <div class="p-6 ${newsFeed[i].read ? 'bg-gray-400' : 'bg-gray-100'} mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
         <div class="flex">
           <div class="flex-auto">
             <a href="#/show/${newsFeed[i].id}">${newsFeed[i].title}</a>  
@@ -90,9 +103,9 @@ function newsDetail() {
         </div>
       </div>
 
-      <div class="h-full border rounded-xl bg-white m-6 p-4 ">
-        <h2>${newsContent.title}</h2>
-        <div class="text-gray-400 h-20">
+      <div class="h-full border rounded-xl bg-white m-6 p-6 ">
+        <h2 class="text-xl font-bold">${newsContent.title}</h2>
+        <div class="text-gray-400 min-h-content">
           ${newsContent.content}
         </div>
 
@@ -101,6 +114,13 @@ function newsDetail() {
       </div>
     </div>
   `;
+
+  for (let i = 0; i < store.feeds.length; i++) {
+    if (store.feeds[i].id === Number(id)) {
+      store.feeds[i].read = true;
+      break;
+    }
+  }
 
   function makeComment(comments, called = 0) {
     const commentString = [];
@@ -113,7 +133,7 @@ function newsDetail() {
             <strong>${comments[i].user}</strong> ${comments[i].time_ago}
           </div>
           <p class="text-gray-700">${comments[i].content}</p>
-        </div>      
+        </div>
       `);
 
       if (comments[i].comments.length > 0) {
